ed05235c894715beb4d70e9ec5d44ad08efd45b1
Code-E-Magpie <279868713+Code-E-Magpie@users.noreply.github.com>
2026-09-02 11:21:51 +0100

    Add files via upload

--- database_toolbox.py
+++ database_toolbox.py
@@ -35,17 +35,17 @@ ADDON_VERSION = ADDON.getAddonInfo('version') # version in addons.xml
 ADDONS = xbmcvfs.translatePath('special://home/addons')
 DATABASE = xbmcvfs.translatePath('special://database/')
 HOME = xbmcvfs.translatePath('special://home/')
-NOTIFICATION_DURATION = ADDON.getSetting('NOTIFICATION_DURATION')
+NOTIFICATION_DURATION = ADDON.getSetting('notification_duration')
 PLUGIN_ID = int(sys.argv[1])
 PLUGIN_URL = sys.argv[0]
-SIZE_HIGHLIGHT = ADDON.getSetting('SIZE_HIGHLIGHT')
-TEXT_ADDON = ADDON.getSetting('TEXT_ADDON')
-TEXT_DARK = ADDON.getSetting('TEXT_DARK')
-TEXT_DIM = ADDON.getSetting('TEXT_DIM')
-TEXT_GENERAL = ADDON.getSetting('TEXT_GENERAL')
-TEXT_HIGHLIGHT = ADDON.getSetting('TEXT_HIGHLIGHT')
-TEXT_ITEM = ADDON.getSetting('TEXT_ITEM')
-TEXT_VALUE = ADDON.getSetting('TEXT_VALUE')
+SIZE_HIGHLIGHT = ADDON.getSetting('size_highlight')
+TEXT_ADDON = ADDON.getSetting('text_addon')
+TEXT_DARK = ADDON.getSetting('text_dark')
+TEXT_DIM = ADDON.getSetting('text_dim')
+TEXT_GENERAL = ADDON.getSetting('text_general')
+TEXT_HIGHLIGHT = ADDON.getSetting('text_highlight')
+TEXT_ITEM = ADDON.getSetting('text_item')
+TEXT_VALUE = ADDON.getSetting('text_value')
 TOOLBOX = os.path.join(ADDON.getAddonInfo('path'), 'resources', 'media', 'toolbox.png')
 USERDATA = xbmcvfs.translatePath('special://userdata/')
 
@@ -180,9 +180,9 @@ INSTRUCTIONS_TEXT = '%s[CR][CR]Open the add-on to access the menu.[CR]Select one
 
 NOTES_TEXT = '[CR][CR][CR]%s[CR][CR]It is important to proceed carefully so changes can be reversed if necessary i.e. \'Clean Addons Database\' closes Kodi without cleanup at the end.[CR][CR]• Backup databases using Kodi file manager or a backup add-on.[CR]• Close other add-ons and save any changes.[CR]• Restart Kodi if required.[CR][CR]ln everyday use the Textures13.db can get corrupted and prevent Kodi starting. Deleting the Textures13.db database resolves this as it rebuilds on startup.[CR]Other databases such as the add-ons database do not rebuild and may require restoring from backup if startup fails.[CR][CR]Later versions of Android make restoring a backedup database difficult.[CR]Android prevents users accessing the Data folder containing data for all the installed apps including Kodi and its databases.[CR]Access is possible using a file explorer app from the Play Store such as Total Commander.[CR]The app needs to be used with the Shizuku app from the Play Store or if restricted from GitHub https://github.com/RikkaApps/Shizuku' % ' '.join('NOTES')
 
-ENVIRONMENT_TEXT = '[CR][CR][CR]%s[CR][CR]Kodi v21.3 Omega apk (Android app) with Confluence skin as default (including default font).[CR]Tablet (1340 x 800 aspect ratio 5:3) running Android 14 using QuickEdit apk (TryItAndSee / LearnAsYouGo iterative development and testing).[CR]Chromecast HD (1280 x 720 aspect ratio 16:9) running Android TV OS version 14 (user testing).[CR]100%% tested and working on Android.[CR]Not tested on other platforms.[CR]Code debugged and reengineered using https://aipy.dev/tools where required.' % ' '.join('DEVELOPMENT ENVIRONMENT')
+ENVIRONMENT_TEXT = '%s[CR][CR]Kodi v21.3 Omega apk (Android app) with Confluence skin as default (including default font).[CR]Tablet (1340 x 800 aspect ratio 5:3) running Android 14 using QuickEdit apk (TryItAndSee / LearnAsYouGo iterative development and testing).[CR]Chromecast HD (1280 x 720 aspect ratio 16:9) running Android TV OS version 14 (user testing).[CR]100%% tested and working on Android.[CR]Not tested on other platforms.[CR]Code debugged and reengineered using https://aipy.dev/tools where required (pre 2.10.0).[CR]Code debugged and reengineered using https://stackoverflow.com/ai-assist (2.10.0 onwards).' % ' '.join('DEVELOPMENT ENVIRONMENT')
 
-CHANGELOG_TEXT = '[CR][CR][CR]%s[LIGHT] (newest at the top)[/LIGHT][CR][CR]Version code x.y.z attributes[CR]x = major change / y = number of \'>\' menu items / z = minor change[CR][CR]version 2.10.2 (10 menu items)[CR]- Clean Addons Database futureproofing added to select Addons*.db[CR]- minor changes to menu formatting[CR][CR]version 2.10.1 (10 menu items)[CR]- added text colour customisation to text boxes and buttons[CR]- added pre clean database size to dialogue boxes (\'Clean Databases (folder)\' options)[CR]- database information formatting reworked and renamed \'Database Files (.db file list) >\'[CR]- added size highlight above value set in settings[CR]- minor changes to improve consistency with other add-ons[CR][CR]version 2.10.0 (10 menu items)[CR]- code added from OpenWizard 2.0.7 by drinfernoo & slamious (plugin.program.openwizard)[CR]- Clean Databases created[CR]- Database Information created[CR]- variables and functions reworked[CR]- menu, multiselect dialogue boxes and logs reworked[CR]- user information updated including instructions and notes[CR]- added user settings for dialogue boxes, notifications, notification duration and trillions of text colour combinations[CR][CR]version 1.3.1 (3 menu items)[CR]- database variable added[CR]- dialogue boxes and logs reworked[CR]- user information updated including instructions and notes[CR][CR]version 1.3.0 (3 menu items)[CR]- initial code from Abacus Program 1.0.0 by %s (plugin.program.code-e-magpie)[CR]- code added from Truncate Tables 1.0.1 by The Cleaner (plugin.program.truncatetables)[CR]- Clean Addons Database created[CR]- icon.png changed and toolbox.png added[CR]- variables and functions reworked[CR]- menu, dialogue boxes and logs reworked[CR]- user information added (instructions, notes, development and changelog)' % (' '.join('CHANGELOG'), ADDON_DEVELOPER)
+CHANGELOG_TEXT = '[CR][CR][CR]%s[LIGHT] (newest at the top)[/LIGHT][CR][CR]Version code x.y.z attributes[CR]x = major change / y = number of \'>\' menu items / z = minor change[CR][CR]version 2.10.3 (10 menu items)[CR]- settings reworked to avoid clashes (different names to variables etc.)[CR][CR]version 2.10.2 (10 menu items)[CR]- Clean Addons Database futureproofing added to select Addons*.db[CR]- minor changes to menu formatting[CR][CR]version 2.10.1 (10 menu items)[CR]- added text colour customisation to text boxes and buttons[CR]- added pre clean database size to dialogue boxes (\'Clean Databases (folder)\' options)[CR]- database information formatting reworked and renamed \'Database Files (.db file list) >\'[CR]- added size highlight above value set in settings[CR]- minor changes to improve consistency with other add-ons[CR][CR]version 2.10.0 (10 menu items)[CR]- code added from OpenWizard 2.0.7 by drinfernoo & slamious (plugin.program.openwizard)[CR]- Clean Databases created[CR]- Database Information created[CR]- variables and functions reworked[CR]- menu, multiselect dialogue boxes and logs reworked[CR]- user information updated including instructions and notes[CR]- added user settings for dialogue boxes, notifications, notification duration and trillions of text colour combinations[CR][CR]version 1.3.1 (3 menu items)[CR]- database variable added[CR]- dialogue boxes and logs reworked[CR]- user information updated including instructions and notes[CR][CR]version 1.3.0 (3 menu items)[CR]- initial code from Abacus Program 1.0.0 by %s (plugin.program.code-e-magpie)[CR]- code added from Truncate Tables 1.0.1 by The Cleaner (plugin.program.truncatetables)[CR]- Clean Addons Database created[CR]- icon.png changed and toolbox.png added[CR]- variables and functions reworked[CR]- menu, dialogue boxes and logs reworked[CR]- user information added (instructions, notes, development and changelog)' % (' '.join('CHANGELOG'), ADDON_DEVELOPER)
 
 User_Information_Text = '[COLOR %s][B]%s[/B][CR][COLOR %s][LIGHT](Instructions / Notes / Development Environment / Changelog)[/LIGHT][/COLOR][/COLOR][CR][CR][COLOR %s]%s[/COLOR]' % (TEXT_ITEM, ' '.join('USER INFORMATION'), TEXT_VALUE, TEXT_GENERAL, (INSTRUCTIONS_TEXT + NOTES_TEXT + ENVIRONMENT_TEXT + CHANGELOG_TEXT))
 
@@ -349,9 +349,9 @@ def Database_Cleaner(database_selected):
 	database_bytes = os.path.getsize(database_selected)
 	database_size = Size_Convert(database_bytes)
 
-	if ADDON.getSetting('NOTIFICATIONS') == 'true':
+	if ADDON.getSetting('notifications') == 'true':
 		Notification(Addon_Title, '[COLOR %s]Clean Databases: %s[/COLOR]' % (TEXT_GENERAL, database))
-	if ADDON.getSetting('DIALOGUE_BOXES') == 'true':
+	if ADDON.getSetting('dialogue_boxes') == 'true':
 		Dialogue.ok(Addon_Title, '[COLOR %s]Clean Databases: [LIGHT](User Information)[/LIGHT][CR][COLOR %s]%s[/COLOR]%s[COLOR %s][LIGHT] (%s)[/LIGHT][/COLOR][CR]%s[/COLOR]' % (TEXT_GENERAL, (TEXT_VALUE if database_bytes < int(SIZE_HIGHLIGHT) else TEXT_HIGHLIGHT), database_size, database, TEXT_DARK, database_size_before, database_selected))
 	Log(Log_Title + Db + 'clean: %s done' % database_selected, xbmc.LOGINFO)
 
